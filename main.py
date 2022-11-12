15d68ca3adf7923c34540f0f5138f71416682ea8
GerantonStorm <gerantonstorm@mail.ru>
2022-11-12 18:50:31 +0500

    решение задачи 2

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+import random
 import sys
 
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
+from ui import Ui_Form
 
 
-class Chess(QWidget, Ui_MainWindow):
+class Chess(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.drawCircle)
 
+    def drawCircle(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor('yellow'))
+            r = random.randint(20, 100)
+            qp.drawEllipse(QPoint(200, 200), r, r)
+            qp.end()
+            self.do_paint = False
 
 
 if __name__ == '__main__':
